8504252dde824858df06c4da96f633f3dc414f4c
Kanesha Patterson <kpatt2216@gmail.com>
2017-10-09 16:46:09 -0500

    upload assets files
    assets upload complete

---
+++ assets/app.js
@@ -0,0 +1,26 @@
+/* particleJS.load(@dom-id, @path-json, @callback (optional)); */
+particlesJS.load('particles-js', 'assets/particles.json', function(){
+    console.log('callback - particles.js config loaded');
+});
+
+(function($){
+		/* ---------------------------------------------- /*
+		 * Progress bars, counters animations
+		/* ---------------------------------------------- */
+
+		$('.progress-bar').each(function(i) {
+			$(this).appear(function() {
+				var percent = $(this).attr('aria-valuenow');
+				$(this).animate({'width' : percent + '%'});
+				$(this).find('span').animate({'opacity' : 1}, 900);
+				$(this).find('span').countTo({from: 0, to: percent, speed: 900, refreshInterval: 30});
+			});
+		});
+
+		$('.counter-item').each(function(i) {
+			$(this).appear(function() {
+				var number = $(this).find('.counter-number').data('number');
+				$(this).find('.counter-number span').countTo({from: 0, to: number, speed: 1200, refreshInterval: 30});
+			});
+        });
+    })(jQuery);
