96cb00c8045978b2ae0513cc98d7fcec1d8688d1
Kanesha Patterson <kpatt2216@gmail.com>
2017-10-09 16:37:12 -0500

    upload folder called assets with one file codekata

---
+++ assets/codekatas.js
@@ -0,0 +1,11 @@
+
+function repeatStr(str, num)
+    for (var i = 0; i < num; i++){
+        if(num < 0){
+            return "";
+        }else{
+            console.log(str);
+        }
+}
+
+repeatStr("abc",3);
